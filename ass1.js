ff51fb273493593efe178237689ec19c1f6533a8
Oladiti Oluwanifemi Precious <oluwanifemiprecious3@gmail.com>
2026-05-04 10:47:51 +0100

    Add files via upload

---
+++ ass1.js
@@ -0,0 +1,7 @@
+//Declare a variable named 🐰bunny and assign it to an object with the following properties:
+// name, age, and isHappy. Set the value of name to a string, age to a number, and isHappy to a boolean.
+let bunny = {
+  name: "Fluffy",
+  age: 2,
+  isHappy: true
+};
